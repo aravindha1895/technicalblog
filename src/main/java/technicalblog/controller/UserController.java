e149d5142b6dc9b32aeb40cf35778f52b327d491
aravindha1895 <sridharanrvnd@gmail.com>
2019-08-17 17:48:09 +0530

    Registration func

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import technicalblog.model.Post;
 import technicalblog.model.User;
 import technicalblog.service.PostService;
+import technicalblog.service.UserService;
 
 @Controller
 public class UserController {
@@ -17,6 +18,9 @@ public class UserController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping("users/login")
     public String login() {
         return "users/login";
@@ -27,9 +31,19 @@ public class UserController {
         return "users/registration";
     }
 
+    @RequestMapping(value = "users/registration", method=RequestMethod.POST)
+    public String registerUser(User user) {
+        return "users/login";
+    }
+    
     @RequestMapping(value = "users/login", method=RequestMethod.POST)
     public String loginUser(User user) {
-        return "redirect:/posts";
+        if(userService.login(user)) {
+            return "redirect:/posts";
+        }
+        else {
+            return "users/login";
+        }
     }
 
     @RequestMapping(value = "users/logout", method=RequestMethod.POST)
